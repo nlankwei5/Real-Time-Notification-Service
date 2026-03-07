0094ae33199bb3d49069d4ddca2e6227d35dcfc0
Ebenezer Lamptey <87435572+nlankwei5@users.noreply.github.com>
2026-03-07 14:33:12 +0000

    Add Event serializer for the event model

---
+++ core/serializers.py
@@ -0,0 +1,11 @@
+from rest_framework import serializers
+from .models import Event
+
+
+
+
+class Event(serializers.ModelSerializer):
+    class Meta:
+        model = Event 
+        fields = ['actor', 'source', 'event_type','object_type', 'object_id', 'metadata', 'created_at']
+        read_only_fields = ['created_at']
